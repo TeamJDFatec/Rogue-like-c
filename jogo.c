b01ee50caf84f927c8bb70322070803910d6798f
victorCatharina <matheusvictor5019@gmail.com>
2022-05-26 21:28:12 -0300

    movimentar o monstro

--- jogo.c
+++ jogo.c
@@ -9,13 +9,16 @@ int main() {
 
     srand(time(NULL));
 
-    int ox = MAX_COLUNA / 2, oy = MAX_LINHA - 2;
-    int px = 1, py = 1;
-    int mx = (int)(MAX_COLUNA * 0.7), my = (int)(MAX_LINHA * 0.7);
+    int ox = MAX_COLUNA / 2, oy = MAX_LINHA - 2; //porta de saída
+    int px = 1, py = 1; //personagem
+    int mx = (int)(MAX_COLUNA * 0.7), my = (int)(MAX_LINHA * 0.7); //monstro
     int x, y;
+    int temEspada = 0;
+
+    int aleatoriedade = 0; //variavel necessária para o jeito 2 de fazer o monstro funcionar;
 
     char comando;
-    int movimentoRealizado;
+    //int movimentoRealizado;
 
     for (;;){
 
@@ -43,21 +46,21 @@ int main() {
         printf("Sua vez!. Digite um comando: ");
         scanf(" %c", &comando);
 
-        movimentoRealizado = 1;
+        //movimentoRealizado = 1;
         switch(comando){
             case 'c':
                 py = py - 1;
                 if (py < 1){
                     py = 1;
-                    movimentoRealizado = 0;
+                    //movimentoRealizado = 0;
                 }
                 break;
 
             case 'b':
                 py += 1;
-                if (py >= 10 - 1){
-                    py = 10 - 2;
-                    movimentoRealizado = 0;
+                if (py >= MAX_LINHA - 1){
+                    py = MAX_LINHA - 2;
+                    //movimentoRealizado = 0;
                 }
                 break;
 
@@ -65,23 +68,147 @@ int main() {
                 px -= 1;
                 if (px < 1){
                     px = 1;
-                    movimentoRealizado = 0;
+                    //movimentoRealizado = 0;
                 }
                 break;
 
             case 'd':
                 px += 1;
-                if (px >= 10 - 1){
-                    px = 10 - 2;
-                    movimentoRealizado = 0;
+                if (px >= MAX_COLUNA - 1){
+                    px = MAX_COLUNA - 2;
+                    //movimentoRealizado = 0;
                 }
                 break;
 
             default:
-                movimentoRealizado = 0;
+                //movimentoRealizado = 0;
                 printf("Comando invalido!\n");
                 printf("Pressione uma tecla para continuar!\n");
                 system("PAUSE");
         }
+
+        //inserir aqui o código do monstro perseguindo o personagem...
+
+        if(px != mx){
+
+            if(px < mx) {
+            mx = mx - 1;
+            }else if(px > mx) {
+                mx = mx + 1;
+            }else {
+                mx = mx;
+            }
+
+        }else if(mx == px){
+
+            if(py < my) {
+            my = my - 1;
+            }else if(py > my) {
+                my = my + 1;
+            }else {
+                my = my;
+            }
+
+        }
+
+        /*----------------------------------------------------------------------------------------*/
+
+        //inserir aqui o código do monstro andando aleatoriamente
+
+        /*aleatoriedade = rand()%120;
+
+        if(aleatoriedade <= 10) {
+            mx = mx - 1;
+
+            if (mx < 1){
+                mx = 1;
+                //movimentoRealizado = 0;
+            }
+
+        }else if(aleatoriedade > 10 && aleatoriedade <= 20) {
+            mx = mx + 1;
+
+            if (mx >= MAX_COLUNA - 1){
+                mx = MAX_COLUNA - 2;
+                //movimentoRealizado = 0;
+            }
+
+        }else if(aleatoriedade > 20 && aleatoriedade <= 30) {
+            my = my - 1;
+
+            if (my < 1){
+                my = 1;
+                //movimentoRealizado = 0;
+            }
+
+        }else if(aleatoriedade > 30 && aleatoriedade <= 40) {
+            my = my + 1;
+
+            if (my >= MAX_LINHA - 1){
+                my = MAX_LINHA - 2;
+                //movimentoRealizado = 0;
+            }
+        }
+
+        else if(aleatoriedade > 40 && aleatoriedade <= 50) {
+            mx = mx - 1;
+
+            if (mx < 1){
+                mx = 1;
+                //movimentoRealizado = 0;
+            }
+        }else if(aleatoriedade > 50 && aleatoriedade <= 60) {
+            mx = mx + 1;
+
+            if (mx >= MAX_COLUNA - 1){
+                mx = MAX_COLUNA - 2;
+                //movimentoRealizado = 0;
+            }
+        }else if(aleatoriedade > 60 && aleatoriedade <= 70) {
+            my = my - 1;
+
+            if (my < 1){
+                my = 1;
+                //movimentoRealizado = 0;
+            }
+        }else if(aleatoriedade > 70 && aleatoriedade <= 80) {
+            my = my + 1;
+
+            if (my >= MAX_LINHA - 1){
+                my = MAX_LINHA - 2;
+                //movimentoRealizado = 0;
+            }
+        }
+
+        else if(aleatoriedade > 80 && aleatoriedade <= 90) {
+            mx = mx - 1;
+
+            if (mx < 1){
+                mx = 1;
+                //movimentoRealizado = 0;
+            }
+        }else if(aleatoriedade > 90 && aleatoriedade <= 100) {
+            mx = mx + 1;
+
+            if (mx >= MAX_COLUNA - 1){
+                mx = MAX_COLUNA - 2;
+                //movimentoRealizado = 0;
+            }
+        }else if(aleatoriedade > 100 && aleatoriedade <= 110) {
+            my = my - 1;
+
+            if (my < 1){
+                my = 1;
+                //movimentoRealizado = 0;
+            }
+        }else if(aleatoriedade > 110 && aleatoriedade <= 120) {
+            my = my + 1;
+
+            if (my >= MAX_LINHA - 1){
+                my = MAX_LINHA - 2;
+                //movimentoRealizado = 0;
+            }
+        }*/
+
     }
 }
