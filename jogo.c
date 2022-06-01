2edb2692e3812d03e802d1e2e25015aed9559d8c
victorCatharina <matheusvictor5019@gmail.com>
2022-06-01 19:23:08 -0300

    Subindo alterações pra teste

--- jogo.c
+++ jogo.c
@@ -1,7 +1,12 @@
+#define _WIN32_WINNT 0x0500
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <conio.h>
+#include <time.h>
+#include <stdbool.h>
+#include <windows.h>
+#include<dos.h>
 
 #define MAX_LINHA 20
 #define MAX_COLUNA 20
@@ -17,46 +22,45 @@ char maiusculo(char comando){
     return comando;
 }
 
+
 int geraNumAleatorio(char eixo){
 
     srand(time(NULL));
-    int nAleatorio = rand()%120;
+    float nAleatorio = rand()%20 + 1;
 
-    if (eixo == 'X'){
-        if(nAleatorio >= MAX_COLUNA){
-            return nAleatorio - MAX_COLUNA;
-        } else if (nAleatorio < MAX_COLUNA){
-            return MAX_COLUNA - nAleatorio;
-        } else {
-            return nAleatorio;
+    if (eixo == 'X' && nAleatorio >= MAX_COLUNA -1){
+        while (nAleatorio >= MAX_COLUNA -1){
+            nAleatorio = rand()%120;
+            nAleatorio = (MAX_COLUNA * (MAX_COLUNA / nAleatorio));
         }
-    } else if (eixo == 'Y'){
-        if (nAleatorio >= MAX_LINHA){
-            return nAleatorio - MAX_LINHA;
-        } else if (nAleatorio < MAX_LINHA) {
-            return MAX_LINHA - nAleatorio;
-        } else {
-            return nAleatorio;
+    } else if (eixo == 'Y' && nAleatorio >= MAX_LINHA -1){
+        while(nAleatorio >= MAX_LINHA -1){
+            nAleatorio = rand()%120;
+            nAleatorio = (MAX_LINHA * (MAX_LINHA / nAleatorio));
         }
     }
 
+
+    return (int)nAleatorio;
 }
 
-int main() {
+int main(void) {
 
-    char comando;
+    maximize_window();
+    hide_cursor();
 
     int x, y;
     int ox = MAX_COLUNA / 2, oy = MAX_LINHA - 2; //porta de saída
     int px = 1, py = 1; //personagem
-    int mx = geraNumAleatorio('X'), my = geraNumAleatorio('Y');
-    int temEspada = 0;
+    int mx = 10, my = 10;
+    char comando;
 
-    for (;;){
-
+    bool vivo = true;
+    do{
         system("CLS");
 
         for (y = 0; y < MAX_LINHA; y++){
+            printf("\t\t\t");
             for (x = 0; x < MAX_COLUNA; x++){
                 if (y == 0 || x == 0 || x == MAX_COLUNA - 1 || y == MAX_LINHA - 1)
                     printf("#");
@@ -73,13 +77,9 @@ int main() {
         }
         printf("\n");
 
-
-        printf("Comando: W (cima)\tD (direita)\tS (baixo)\tA (esquerda)\n");
-        printf("Sua vez!. Digite um comando: ");
         comando = getch();
         comando = maiusculo(comando);
 
-        //movimentoRealizado = 1;
         switch(comando){
             case 'W':
                 py = py - 1;
@@ -120,132 +120,21 @@ int main() {
                 system("PAUSE");
         }
 
-        movMonstro(px, py);
-
-        //inserir aqui o código do monstro perseguindo o personagem...
-
-       /* if(px != mx){
-
-            if(px < mx) {
-            mx = mx - 1;
-            }else if(px > mx) {
-                mx = mx + 1;
-            }else {
-                mx = mx;
-            }
-
-        }else if(mx == px){
-
-            if(py < my) {
-            my = my - 1;
-            }else if(py > my) {
-                my = my + 1;
-            }else {
-                my = my;
-            }
-
-        }
-*/
-        /*----------------------------------------------------------------------------------------*/
-
-        //inserir aqui o código do monstro andando aleatoriamente
-
-        //aleatoriedade = rand()%120;
 
 
-        /*
-        if(aleatoriedade <= 10) {
-            mx = mx - 1;
+    }while(vivo);
 
-            if (mx < 1){
-                mx = 1;
-                //movimentoRealizado = 0;
-            }
+}
 
-        }else if(aleatoriedade > 10 && aleatoriedade <= 20) {
-            mx = mx + 1;
 
-            if (mx >= MAX_COLUNA - 1){
-                mx = MAX_COLUNA - 2;
-                //movimentoRealizado = 0;
-            }
+void hide_cursor(bool hide)
+{
+  CONSOLE_CURSOR_INFO cursor = {1, FALSE};
+  SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor);
+}
 
-        }else if(aleatoriedade > 20 && aleatoriedade <= 30) {
-            my = my - 1;
-
-            if (my < 1){
-                my = 1;
-                //movimentoRealizado = 0;
-            }
-
-        }else if(aleatoriedade > 30 && aleatoriedade <= 40) {
-            my = my + 1;
-
-            if (my >= MAX_LINHA - 1){
-                my = MAX_LINHA - 2;
-                //movimentoRealizado = 0;
-            }
-        }
-
-        else if(aleatoriedade > 40 && aleatoriedade <= 50) {
-            mx = mx - 1;
-
-            if (mx < 1){
-                mx = 1;
-                //movimentoRealizado = 0;
-            }
-        }else if(aleatoriedade > 50 && aleatoriedade <= 60) {
-            mx = mx + 1;
-
-            if (mx >= MAX_COLUNA - 1){
-                mx = MAX_COLUNA - 2;
-                //movimentoRealizado = 0;
-            }
-        }else if(aleatoriedade > 60 && aleatoriedade <= 70) {
-            my = my - 1;
-
-            if (my < 1){
-                my = 1;
-                //movimentoRealizado = 0;
-            }
-        }else if(aleatoriedade > 70 && aleatoriedade <= 80) {
-            my = my + 1;
-
-            if (my >= MAX_LINHA - 1){
-                my = MAX_LINHA - 2;
-                //movimentoRealizado = 0;
-            }
-        }
-
-        else if(aleatoriedade > 80 && aleatoriedade <= 90) {
-            mx = mx - 1;
-
-            if (mx < 1){
-                mx = 1;
-                //movimentoRealizado = 0;
-            }
-        }else if(aleatoriedade > 90 && aleatoriedade <= 100) {
-            mx = mx + 1;
-
-            if (mx >= MAX_COLUNA - 1){
-                mx = MAX_COLUNA - 2;
-                //movimentoRealizado = 0;
-            }
-        }else if(aleatoriedade > 100 && aleatoriedade <= 110) {
-            my = my - 1;
-
-            if (my < 1){
-                my = 1;
-                //movimentoRealizado = 0;
-            }
-        }else if(aleatoriedade > 110 && aleatoriedade <= 120) {
-            my = my + 1;
-
-            if (my >= MAX_LINHA - 1){
-                my = MAX_LINHA - 2;
-                //movimentoRealizado = 0;
-            }
-        }*/
-
-    }
+void maximize_window()
+{
+    HWND consoleWindow = GetConsoleWindow();
+    ShowWindow(consoleWindow, SW_MAXIMIZE);
 }
